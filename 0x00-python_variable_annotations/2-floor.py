543e34e6c19055854f51414e514210143224f9b1
james-eo <jameseokonkwo@gmail.com>
2024-07-04 16:57:16 +0100

    Function returns floor of float

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,16 +1,11 @@
-#!/usr/bin/python3
-"""This module contains the function that returns the floor of a float."""
+#!/usr/bin/env python3
+"""
+This module contains a function that returns the floor of a float.
+"""
 
 import math
 
 
 def floor(n: float) -> int:
-    """Returns the floor of a float.
-
-    Args:
-        n (float): The float number.
-
-    Returns:
-        int: The floor of the float number.
-    """
+    """Return the floor of a float."""
     return math.floor(n)
